53f47f607a7bf01d25a79d76d0ae925a2b9ff837
John Hiott <jhiott@oolcay.com>
2014-05-14 09:09:29 -0400

    LocationActivty now ListActivity

--- src/com/oolcay/weather/LocationActivity.java
+++ src/com/oolcay/weather/LocationActivity.java
@@ -1,13 +1,22 @@
 package com.oolcay.weather;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class LocationActivity extends Activity {
+public class LocationActivity extends ListActivity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
 
   }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+  }
+
 }
 
