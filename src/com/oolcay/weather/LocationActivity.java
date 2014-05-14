d4939be8886467726ed008c4ae3ec4d1de9014b7
John Hiott <jhiott@oolcay.com>
2014-05-14 07:19:46 -0400

    added location activity

---
+++ src/com/oolcay/weather/LocationActivity.java
@@ -0,0 +1,13 @@
+package com.oolcay.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocationActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+
+  }
+}
+
