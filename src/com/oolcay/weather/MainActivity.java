7bd87480d69d156256e9db7d487a4c08bf0f49dc
John Hiott <jhiott@oolcay.com>
2014-05-15 07:29:45 -0400

    get location from config, remove old graph test code

--- src/com/oolcay/weather/MainActivity.java
+++ src/com/oolcay/weather/MainActivity.java
@@ -2,33 +2,60 @@ package com.oolcay.weather;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.Toast;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
+import com.oolcay.weather.Models.Location;
 import com.oolcay.weather.Network.Request;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends Activity {
+
+  private ForecastApplication state;
+  private DatabaseHandler mDatabaseHandler;
+  private double mLat;
+  private double mLon;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    //GetWeather getWeather = new GetWeather();
-    //getWeather.execute();
+    state = ((ForecastApplication) getApplicationContext());
+    Location location = state.getCurrentLocation();
+
+    mDatabaseHandler = new DatabaseHandler(this);
+
+    ConfigHelper configHelper = new ConfigHelper(this);
+
+    List <Location> locations = mDatabaseHandler.getAllLocations();
+
+    if (locations.size() == 0){
+
+    }else{
+      if (location == null){
+        state.setCurrentLocation(mDatabaseHandler.getLocation(configHelper.getHomeLocation()));
+      }
+    }
+
+    mLat = state.getCurrentLocation().getLat();
+    mLon = state.getCurrentLocation().getLon();
+
+    Toast.makeText(this, " " + state.getCurrentLocation().getName(), Toast.LENGTH_LONG).show();
+
+    GetWeather getWeather = new GetWeather();
+    getWeather.execute();
+  }
+
+  @Override
+  public void onResume(){
+    super.onResume();
+    state = ((ForecastApplication) getApplicationContext());
   }
 
   private void handleResponse(JSONObject results){
@@ -37,38 +64,13 @@ public class MainActivity extends Activity {
       Toast.makeText(this, "Error Loading Weather Data", Toast.LENGTH_SHORT);
     }
 
-    GraphView.GraphViewData[] points = new GraphView.GraphViewData[0];
-
     try {
      JSONObject hourly = results.getJSONObject("hourly");
      JSONArray hourly_data = hourly.getJSONArray("data");
 
-     int length = hourly_data.length();
-     points = new GraphView.GraphViewData[length];
-
-      for (int x = 0; x < length; x++){
-        JSONObject data_point =  hourly_data.getJSONObject(x);
-        int time = data_point.getInt("time");
-        double temp = data_point.getDouble("temperature");
-        points[x] = new GraphView.GraphViewData(time, temp);
-      }
     } catch (JSONException e) {
       Log.e("WEATHER_APP: ", e.toString());
     }
-
-    GraphView graphView = new LineGraphView(
-        this // context
-        , "GraphViewDemo" // heading
-    );
-
-    graphView.getGraphViewStyle().setNumHorizontalLabels(1);
-    graphView.getGraphViewStyle().setNumVerticalLabels(5);
-    graphView.setHorizontalLabels(new String[] {"12:01 am", "12:00 pm", "11:59 pm"});
-
-    graphView.addSeries(new GraphViewSeries(points));
-    LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
-    layout.addView(graphView);
-
   }
 
   public void openLocations(View v){
@@ -83,7 +85,7 @@ public class MainActivity extends Activity {
       JSONObject weatherData = null;
       try{
         Request request = new Request();
-        request.setUrl("https://api.forecast.io/forecast//34.988,-81.237");
+        request.setUrl(Constants.FORECAST_URL + Constants.FORECAST_KEY + "/" + mLat + "," + mLon);
         weatherData = request.getJsonResponse();
       }catch (Exception e){
         Log.e("WEATHER", e.toString());
@@ -93,7 +95,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onPostExecute(JSONObject result){
-      handleResponse(result);
+      if (result != null)
+        handleResponse(result);
     }
   }
 }
