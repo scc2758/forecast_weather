57c8b7610cbcc71b72bba769cf71df5313703a6d
John Hiott <jhiott@oolcay.com>
2014-05-16 07:09:42 -0400

    refactor: extra id now constant

--- src/com/oolcay/weather/LocationFragment.java
+++ src/com/oolcay/weather/LocationFragment.java
@@ -9,6 +9,8 @@ import com.oolcay.weather.Models.Location;
 
 public class LocationFragment extends Fragment {
 
+  public static final String EXTRA_LOCATION_ID = "extra_location_id";
+
   private Location mLocation;
 
   @Override
@@ -21,6 +23,16 @@ public class LocationFragment extends Fragment {
       Bundle savedInstanceState){
     View v = inflater.inflate(R.layout.location_fragment, parent, false);
     return v;
+  }
+
+  public static LocationFragment newInstance(int locationId){
+    Bundle args = new Bundle();
+    args.putInt(EXTRA_LOCATION_ID, locationId);
+
+    LocationFragment fragment = new LocationFragment();
+    fragment.setArguments(args);
+
+    return fragment;
   }
 
 }
