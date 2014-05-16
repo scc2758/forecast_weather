4abfbf72e23d06e66bbd3dfc53bd840e6b51c3c2
John Hiott <jhiott@oolcay.com>
2014-05-16 16:04:03 -0400

    added calls the the weather api

--- src/com/oolcay/weather/LocationFragment.java
+++ src/com/oolcay/weather/LocationFragment.java
@@ -1,19 +1,27 @@
 package com.oolcay.weather;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.oolcay.weather.Models.Location;
+import com.oolcay.weather.Network.Request;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LocationFragment extends Fragment {
 
   public static final String EXTRA_LOCATION_ID = "extra_location_id";
 
   private Location mLocation;
+  private Context mContext;
 
   @Override
   public void onCreate(Bundle savedInstanceState){
@@ -21,9 +29,9 @@ public class LocationFragment extends Fragment {
 
     int id = getArguments().getInt(LocationFragment.EXTRA_LOCATION_ID);
 
-    Context context = getActivity();
+    mContext = getActivity();
 
-    ForecastApplication state = (ForecastApplication)context.getApplicationContext();
+    ForecastApplication state = (ForecastApplication)mContext.getApplicationContext();
 
     mLocation = state.getAllLocations().get(id);
 
@@ -37,6 +45,9 @@ public class LocationFragment extends Fragment {
     TextView textView = (TextView)v.findViewById(R.id.summary);
     textView.setText(mLocation.getName());
 
+    GetWeather getWeather = new GetWeather(v);
+    getWeather.execute();
+
     return v;
   }
 
@@ -48,5 +59,52 @@ public class LocationFragment extends Fragment {
     fragment.setArguments(args);
 
     return fragment;
+  }
+
+  private void handleResponse(JSONObject results, View v ){
+
+    if (results == null){
+      Toast.makeText(mContext, "Error Loading Weather Data", Toast.LENGTH_SHORT);
+    }
+
+    try {
+      JSONObject hourly = results.getJSONObject("currently");
+      String temperature = hourly.getString("temperature");
+
+      TextView textView = (TextView)v.findViewById(R.id.summary);
+
+      textView.setText(temperature);
+
+    } catch (JSONException e) {
+      Log.e("WEATHER_APP: ", e.toString());
+    }
+  }
+
+  public class GetWeather extends AsyncTask<Object, Void, JSONObject> {
+
+    private View mView;
+
+    GetWeather(View v){
+      mView = v;
+    }
+
+    @Override
+    protected JSONObject doInBackground(Object... params) {
+      JSONObject weatherData = null;
+      try{
+        Request request = new Request();
+        request.setUrl(Constants.FORECAST_URL + Constants.FORECAST_KEY + "/" + mLocation.getLat() + "," + mLocation.getLon());
+        weatherData = request.getJsonResponse();
+      }catch (Exception e){
+        Log.e("WEATHER", e.toString());
+      }
+      return weatherData;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject result){
+      if (result != null)
+        handleResponse(result, mView);
+    }
   }
 }
