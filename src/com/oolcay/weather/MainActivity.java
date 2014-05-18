1ef3ddc6d9429550646c25046df70fe0a0f9802d
John Hiott <jhiott@oolcay.com>
2014-05-18 10:03:54 -0400

    loading weather data b4 viewpager is setup

--- src/com/oolcay/weather/MainActivity.java
+++ src/com/oolcay/weather/MainActivity.java
@@ -12,11 +12,15 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.oolcay.weather.Models.Location;
 import com.oolcay.weather.Network.Request;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 public class MainActivity extends FragmentActivity {
 
@@ -30,10 +34,10 @@ public class MainActivity extends FragmentActivity {
 
     Context context = getApplicationContext();
     state = ((ForecastApplication) getApplicationContext());
+    setContentView(R.layout.main);
 
     mViewPager = new ViewPager(this);
     mViewPager.setId(R.id.viewPager);
-    setContentView(R.layout.main);
 
     FrameLayout layout = (FrameLayout) findViewById(R.id.fragmentContainer);
     layout.addView(mViewPager);
@@ -41,7 +45,36 @@ public class MainActivity extends FragmentActivity {
     mDatabaseHandler = new DatabaseHandler(context);
     state.setAllLocations(mDatabaseHandler.getAllLocations());
 
+    GetWeather getWeather = new GetWeather(mDatabaseHandler.getAllLocations());
+    getWeather.execute();
+
+  }
+
+  @Override
+  public void onResume(){
+    super.onResume();
+    state = ((ForecastApplication) getApplicationContext());
+  }
+
+  @Override
+  public void onPause(){
+    super.onPause();
+    state.setCurrentLocation(state.getAllLocations().get(mViewPager.getCurrentItem()).getId());
+  }
+
+  public void openLocations(View v){
+    Intent intent = new Intent(this, LocationActivity.class);
+    startActivity(intent);
+  }
+
+  private void handleResponse(List<Location> results){
+    state.setAllLocations(results);
+
+    ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+    progressBar.setVisibility(View.GONE);
+
     FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
       @Override
@@ -55,7 +88,7 @@ public class MainActivity extends FragmentActivity {
       }
     });
 
-    ConfigHelper configHelper = new ConfigHelper(context);
+    ConfigHelper configHelper = new ConfigHelper(getApplicationContext());
 
     int locationId = state.getCurrentLocation();
 
@@ -75,63 +108,47 @@ public class MainActivity extends FragmentActivity {
     }
   }
 
-  @Override
-  public void onResume(){
-    super.onResume();
-    state = ((ForecastApplication) getApplicationContext());
-  }
+  public class GetWeather extends AsyncTask<Object, Void, List<Location>> {
 
-  @Override
-  public void onPause(){
-    super.onPause();
-    state.setCurrentLocation(state.getAllLocations().get(mViewPager.getCurrentItem()).getId());
-  }
+    private List<Location> mLocations;
 
-  public void openLocations(View v){
-    Intent intent = new Intent(this, LocationActivity.class);
-    startActivity(intent);
-  }
-  private void handleResponse(JSONObject results, int id ){
-
-    try {
-      JSONObject hourly = results.getJSONObject("currently");
-      String temperature = hourly.getString("temperature");
-
-    } catch (JSONException e) {
-      Log.e("WEATHER_APP: ", e.toString());
-    }
-  }
-
-  public class GetWeather extends AsyncTask<Object, Void, JSONObject> {
-
-    private double mLat;
-    private double mLon;
-    private int mId;
-
-    GetWeather(double lat, double lon, int id){
-      mId = id;
-      mLon = lon;
-      mLat = lat;
+    GetWeather(List<Location> list){
+      mLocations = list;
     }
 
     @Override
-    protected JSONObject doInBackground(Object... params) {
+    protected List<Location> doInBackground(Object... params) {
       JSONObject weatherData = null;
-      try{
-        Request request = new Request();
-        request.setUrl(Constants.FORECAST_URL + Constants.FORECAST_KEY + "/" + mLat + "," + mLon);
-        weatherData = request.getJsonResponse();
-      }catch (Exception e){
-        Log.e("WEATHER", e.toString());
+
+      for (int x=0; x< mLocations.size(); x++ ){
+
+        try{
+
+          double lat = mLocations.get(x).getLat();
+          double lon = mLocations.get(x).getLon();
+
+          Request request = new Request();
+
+          request.setUrl(Constants.FORECAST_URL + Constants.FORECAST_KEY + "/" + lat + "," + lon);
+
+          weatherData = request.getJsonResponse();
+
+          JSONObject hourly = weatherData.getJSONObject("currently");
+
+          String temperature = hourly.getString("temperature");
+
+          mLocations.get(x).setLastUpdated(System.currentTimeMillis()/1000L);
+
+        }catch (Exception e){
+          Log.e("WEATHER", e.toString());
+        }
       }
-      return weatherData;
+      return mLocations;
     }
 
     @Override
-    protected void onPostExecute(JSONObject result){
-      if (result != null)
-        handleResponse(result, mId);
+    protected void onPostExecute(List<Location> list){
+        handleResponse(list);
     }
   }
-
 }
