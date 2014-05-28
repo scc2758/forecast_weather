32b599c1c24a7f0e7bf15be85f1cd2fdf158f476
John Hiott <jhiott@oolcay.com>
2014-05-28 07:51:53 -0400

    added fields to weather model

--- src/com/oolcay/weather/Models/Weather.java
+++ src/com/oolcay/weather/Models/Weather.java
@@ -4,25 +4,37 @@ import java.util.List;
 
 public class Weather{
 
-  public final static String CLEAR_DAY = "clear-day";
-  public final static String CLEAR_NIGHT = "clear-night";
-  public final static String RAIN = "rain";
-  public final static String SNOW = "snow";
-  public final static String SLEET = "sleet";
+  //icons
   public final static String WIND = "wind";
   public final static String FOG = "fog";
   public final static String CLOUDY = "cloudy";
   public final static String PARTLY_CLOUDY_DAY = "partly-cloudy_day";
   public final static String PARTLY_CLOUDY_NIGHT = "partly-cloudy-night";
+  public final static String CLEAR_DAY = "clear-day";
+  public final static String CLEAR_NIGHT = "clear-night";
+
+  //icons and preciptype
+  public final static String RAIN = "rain";
+  public final static String SNOW = "snow";
+  public final static String SLEET = "sleet";
 
   private String mSummary;
-  private double mTemperature;
-  private double mFeelsLike;
-  private double mPrecipProbability;
-  private List<Weather> mHourly;
-  private List<Weather> mDaily;
   private int mTime;
   private String mIcon;
+
+  private double mTemperature;    //current, minute, hour
+  private double mFeelsLike;
+  private double temperatureMin;
+  private int temperatureMinTime;
+  private double temperatureMax;
+  private int temperatureMaxTime;
+
+  private double mPrecipProbability;
+  private String precipType;
+
+  private List<Weather> mHourly;
+  private List<Weather> mDaily;
+
 
   public String getSummary() {
     return mSummary;
@@ -86,5 +98,45 @@ public class Weather{
 
   public void setDaily(List<Weather> daily) {
     mDaily = daily;
+  }
+
+  public double getTemperatureMin() {
+    return temperatureMin;
+  }
+
+  public void setTemperatureMin(double temperatureMin) {
+    this.temperatureMin = temperatureMin;
+  }
+
+  public int getTemperatureMinTime() {
+    return temperatureMinTime;
+  }
+
+  public void setTemperatureMinTime(int temperatureMinTime) {
+    this.temperatureMinTime = temperatureMinTime;
+  }
+
+  public double getTemperatureMax() {
+    return temperatureMax;
+  }
+
+  public void setTemperatureMax(double temperatureMax) {
+    this.temperatureMax = temperatureMax;
+  }
+
+  public int getTemperatureMaxTime() {
+    return temperatureMaxTime;
+  }
+
+  public void setTemperatureMaxTime(int temperatureMaxTime) {
+    this.temperatureMaxTime = temperatureMaxTime;
+  }
+
+  public String getPrecipType() {
+    return precipType;
+  }
+
+  public void setPrecipType(String precipType) {
+    this.precipType = precipType;
   }
 }
