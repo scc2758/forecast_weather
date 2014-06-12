75cd83d2cec7c941f112d04f509e86cb66d1d6e7
John Hiott <jhiott@oolcay.com>
2014-06-12 14:22:47 -0400

    removed async task to all locations weather data

--- src/com/oolcay/weather/MainActivity.java
+++ src/com/oolcay/weather/MainActivity.java
@@ -47,15 +47,22 @@ public class MainActivity extends FragmentActivity {
     mDatabaseHandler = new DatabaseHandler(context);
     state.setAllLocations(mDatabaseHandler.getAllLocations());
 
-    GetWeather getWeather = new GetWeather(mDatabaseHandler.getAllLocations());
-    getWeather.execute();
+    ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+    progressBar.setVisibility(View.GONE);
 
-    //TODO: use window size instead
-    Display display = getWindowManager().getDefaultDisplay();
-    Point size = new Point();
-    display.getSize(size);
-    state.setHeight(size.y);
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
+    mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+      @Override
+      public Fragment getItem(int i) {
+        return LocationFragment.newInstance(i);
+
+      }
+      @Override
+      public int getCount() {
+        return state.getAllLocations().size();
+      }
+    });
   }
 
   @Override
@@ -135,94 +142,6 @@ public class MainActivity extends FragmentActivity {
         mViewPager.setCurrentItem(x);
         break;
       }
-    }
-  }
-
-  public class GetWeather extends AsyncTask<Object, Void, List<Location>> {
-
-    private List<Location> mLocations;
-
-    GetWeather(List<Location> list){
-      mLocations = list;
-    }
-
-    @Override
-    protected List<Location> doInBackground(Object... params) {
-      JSONObject weatherData = null;
-      int length = 0;
-
-      for (int x=0; x< mLocations.size(); x++ ){
-
-        try{
-
-          double lat = mLocations.get(x).getLat();
-          double lon = mLocations.get(x).getLon();
-
-          Request request = new Request();
-          request.setUrl(Constants.FORECAST_URL + Constants.FORECAST_KEY + "/" + lat + "," + lon);
-          weatherData = request.getJsonResponse();
-
-          JSONObject currently = weatherData.getJSONObject("currently");
-          String temperature = currently.getString("temperature");
-
-          Weather weather = new Weather();
-          weather.setTemperature(Double.parseDouble(temperature));
-          weather.setSummary(currently.getString("summary"));
-          weather.setIcon(currently.getString("icon"));
-
-          mLocations.get(x).setWeather(weather);
-          mLocations.get(x).setLastUpdated(System.currentTimeMillis() / 1000L);
-
-          //Populate Locations w/ Hourly Weather Data
-          JSONObject hourly = weatherData.getJSONObject("hourly");
-          JSONArray hourly_data = hourly.getJSONArray("data");
-          length = hourly_data.length();
-          List<Weather> hourlyWeatherList = new ArrayList<Weather>();
-          for (int i = 0; i < length; i++){
-            hourlyWeatherList.add(createWeather(hourly_data.getJSONObject(i)));
-          }
-          mLocations.get(x).getWeather().setHourly(hourlyWeatherList);
-
-
-          //get daily weather data
-          JSONObject daily = weatherData.getJSONObject("daily");
-          JSONArray daily_data = daily.getJSONArray("data");
-          List<Weather> dailyWeatherList = new ArrayList<Weather>();
-          length = daily_data.length();
-          for (int i = 0; i < length; i++) {
-              dailyWeatherList.add(createWeather(daily_data.getJSONObject(i)));
-          }
-          mLocations.get(x).getWeather().setDaily(dailyWeatherList);
-
-        }catch (Exception e){
-          Log.e(getString(R.string.error_tag), e.toString());
-        }
-      }
-      return mLocations;
-    }
-
-    @Override
-    protected void onPostExecute(List<Location> list){
-        handleResponse(list);
-    }
-
-    private Weather createWeather(JSONObject dataPoint) throws JSONException{
-      Weather weather = new Weather();
-
-      try {
-      weather.setTemperature(dataPoint.getDouble("temperature"));
-      } catch (Exception e){
-        Log.d(getString(R.string.error_tag), getString(R.string.no_data_in_json));
-      }
-
-      try {
-        weather.setIcon(dataPoint.getString("icon"));
-      } catch (Exception e){
-        Log.d(getString(R.string.error_tag), getString(R.string.no_data_in_json));
-      }
-
-      weather.setTime(dataPoint.getInt("time"));
-      return weather;
     }
   }
 }
