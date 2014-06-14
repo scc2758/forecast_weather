9a0fa1bbea78cdad458c4a60457f38e0cec4e548
John Hiott <jhiott@oolcay.com>
2014-06-14 14:32:11 -0400

    set layout

--- src/com/oolcay/weather/LocationDetailActivity.java
+++ src/com/oolcay/weather/LocationDetailActivity.java
@@ -7,6 +7,8 @@ public class LocationDetailActivity extends Activity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.location_detail);
 
   }
 
